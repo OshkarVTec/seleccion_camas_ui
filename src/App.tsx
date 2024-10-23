1bd0035ddd7cb14604fb3bc9d14062f7a7d22a81
OshkarVTec <a01275287@tec.mx>
2024-10-22 18:17:34 -0600

    add functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,45 @@
+import { useState } from "react";
 import PageTitle from "./components/common/PageTitle";
 import Text from "./components/common/Text";
+import Header from "./components/landing/Header";
 
-function App() {
+export default function App() {
+	const [selectedFile, setSelectedFile] = useState<File | null>(null);
+	const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		if (event.target.files && event.target.files.length > 0) {
+			setSelectedFile(event.target.files[0]);
+		}
+	};
+
+	const handleUpload = () => {
+		if (selectedFile) {
+			// Handle the file upload logic here
+			console.log("Uploading:", selectedFile.name);
+		}
+	};
 	return (
 		<main className="bg-white flex flex-col p-10">
+			<Header />
 			<div className="my-10">
 				<PageTitle>Instrucciones</PageTitle>
 				<Text>Texto</Text>
 			</div>
 			<div className="my-10">
 				<PageTitle>Selección de fotos</PageTitle>
+				<input type="file" onChange={handleFileChange} />
 			</div>
 			<div className="my-10">
 				<PageTitle>Selección de camas</PageTitle>
+				{selectedFile && (
+					<div>
+						<img
+							src={URL.createObjectURL(selectedFile)}
+							alt="Selected"
+							className="max-w-full h-auto"
+						/>
+					</div>
+				)}
 			</div>
 		</main>
 	);
 }
-
-export default App;
