484a20a29b9130c0f10ae8960f02b85e9fa646bd
OshkarVTec <a01275287@tec.mx>
2024-10-23 10:19:48 -0600

    add save button

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import PageTitle from "./components/common/PageTitle";
 import Text from "./components/common/Text";
 import Header from "./components/landing/Header";
+import ActionButton from "./components/common/ActionButton";
 
 export default function App() {
 	const [selectedFile, setSelectedFile] = useState<File | null>(null);
 	const [numberOfBeds, setNumberOfBeds] = useState<number>(0);
 	const [bedWidth, setBedWidth] = useState<number>(0);
+	const [saved, setSaved] = useState<boolean>(false);
 
 	const handleNumberOfBedsChange = (
 		event: React.ChangeEvent<HTMLInputElement>
@@ -35,52 +37,67 @@ export default function App() {
 			setSelectedFile(event.target.files[0]);
 		}
 	};
+	const handleSave = () => {
+		setSaved(true);
+	};
 
 	return (
 		<>
 			{" "}
 			<Header />
 			<main className="bg-white flex flex-col p-10">
-				<div className="my-10">
+				<div className="flex flex-col gap-2 my-10">
 					<PageTitle>Instrucciones</PageTitle>
 					<Text>Texto</Text>
 				</div>
-				<div className="my-10">
+				<div className="flex flex-col gap-2 my-10">
 					<PageTitle>Selección de fotos</PageTitle>
 					<input type="file" onChange={handleFileChange} />
 				</div>
-				<div className="my-10">
+				<div className="flex flex-col gap-2 my-10">
 					<PageTitle>Selección de camas</PageTitle>
-					{selectedFile && (
-						<div className="flex gap-4">
-							<img
-								src={URL.createObjectURL(selectedFile)}
-								alt="Selected"
-								className="max-w-full h-auto"
-							/>
-							<div className="flex flex-col justify-center gap-10">
-								<label className="flex flex-col justify-center gap-2">
-									Número de camas
-									<input
-										value={numberOfBeds}
-										id="beds"
-										type="number"
-										placeholder="Número de camas"
-										onChange={handleNumberOfBedsChange}
-									/>
-								</label>
-								<label className="flex flex-col justify-center gap-2">
-									Ancho de las camas
-									<input
-										type="range"
-										min="30"
-										max="700"
-										value={bedWidth}
-										onChange={handleBedWidthChange}
-									/>
-								</label>
-							</div>
+					{saved ? (
+						<div className="flex flex-col gap-2 my-10">
+							<PageTitle>¡Listo!</PageTitle>
+							<Text>
+								Se ha guardado tu selección, el modelo está listo. Ya puedes
+								cerrar esta aplicación. Si quieres empezar de nuevo, selecciona
+								otra imagen.{" "}
+							</Text>
 						</div>
+					) : (
+						selectedFile && (
+							<div className="flex gap-4 justify-around">
+								<img
+									src={URL.createObjectURL(selectedFile)}
+									alt="Selected"
+									className="w-3/4 h-auto"
+								/>
+								<div className="flex flex-col justify-center gap-10">
+									<label className="flex flex-col justify-center gap-2 p-2 border-2 border-solid border-green-800 rounded-xl">
+										Número de camas
+										<input
+											value={numberOfBeds}
+											id="beds"
+											type="number"
+											placeholder="Número de camas"
+											onChange={handleNumberOfBedsChange}
+										/>
+									</label>
+									<label className="flex flex-col justify-center gap-2  p-2 border-2 border-solid border-green-800 rounded-xl">
+										Ancho de las camas
+										<input
+											type="range"
+											min="30"
+											max="700"
+											value={bedWidth}
+											onChange={handleBedWidthChange}
+										/>
+									</label>
+									<ActionButton onClick={handleSave}>Guardar</ActionButton>
+								</div>
+							</div>
+						)
 					)}
 				</div>
 			</main>
--- src/components/common/ActionButton.tsx
+++ src/components/common/ActionButton.tsx
@@ -1,7 +1,14 @@
 import { ReactNode } from "react";
 
 export default function ActionButton({ children, onClick }: ActionButtonProps) {
-	return <button onClick={onClick}>{children}</button>;
+	return (
+		<button
+			onClick={onClick}
+			className="p-4 bg-green-700 text-white font-bold rounded-xl"
+		>
+			{children}
+		</button>
+	);
 }
 
 interface ActionButtonProps {
