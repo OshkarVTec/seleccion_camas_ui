47f5b9a42bf3ea1c76480595ceb712585d657a2e
OshkarVTec <a01275287@tec.mx>
2024-10-22 18:04:24 -0600

    add sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import PageTitle from "./components/common/PageTitle";
+import Text from "./components/common/Text";
+
 function App() {
 	return (
-		<>
-			<>Hello world!</>
-		</>
+		<main className="bg-white flex flex-col p-10">
+			<div className="my-10">
+				<PageTitle>Instrucciones</PageTitle>
+				<Text>Texto</Text>
+			</div>
+			<div className="my-10">
+				<PageTitle>Selección de fotos</PageTitle>
+			</div>
+			<div className="my-10">
+				<PageTitle>Selección de camas</PageTitle>
+			</div>
+		</main>
 	);
 }
 
