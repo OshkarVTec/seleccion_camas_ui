c0dbe80c0148c79737ed019e5cd58c11ab9ef792
OshkarVTec <a01275287@tec.mx>
2024-11-26 10:06:01 -0600

    linux deployment

--- forge.config.cjs
+++ forge.config.cjs
@@ -1,23 +1,37 @@
 const { FusesPlugin } = require("@electron-forge/plugin-fuses");
 const { FuseV1Options, FuseVersion } = require("@electron/fuses");
+const path = require("path");
 
 module.exports = {
 	packagerConfig: {
 		asar: true,
+		icon: path.join(__dirname, "assets", "icons", "icon"), // Specify the base name of the icon files without extension
+		executableName: "seleccion_camas", // Specify the name of the executable
 	},
 	rebuildConfig: {},
 	makers: [
 		{
 			name: "@electron-forge/maker-squirrel",
-			config: {},
+			config: {
+				name: "aplicacion_seleccion_camas_windows",
+				setupIcon: path.join(__dirname, "assets", "icons", "icon.ico"), // Path to the icon for the installer
+			},
 		},
 		{
 			name: "@electron-forge/maker-zip",
 			platforms: ["darwin"],
+			config: {
+				name: "AplicacionSeleccionCamas", // Name for macOS executable
+			},
 		},
 		{
 			name: "@electron-forge/maker-deb",
-			config: {},
+			config: {
+				options: {
+					name: "aplicacion_seleccion_camas_linux",
+					icon: path.join(__dirname, "assets", "icons", "icon.png"), // Path to the icon for the installer
+				},
+			},
 		},
 	],
 	plugins: [
